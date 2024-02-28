b2c8d98fc87edc6f53ce5f70300e21b5fc7a7a1e
Youssef El fadi <63858675+youssef-yo@users.noreply.github.com>
2024-02-28 11:24:20 +0100

    Implemented factor agglomeration
    - We suppose that we want one agglomeration of obstacles.
    - Factor of agglomeration = [0, 1] and moltiplied the number of obstacles in the agglomeration gives us
        the number of obstacles in the agglomeration.
    - The ramaining obstacles will be placed randomly in the grid (so it can happen that they will be placed in the agglomeration as well, but it is not a problem)

--- pathfinding/generator/gridGenerator.py
+++ pathfinding/generator/gridGenerator.py
@@ -5,9 +5,45 @@ import math
 # 0 -> free cell
 # 1 -> obstacle
 
-def addObstacles(grid, nObstacle):
+def createAgglomeration(grid, nObstaclesInAgglomeration):
+    """"
+    Create an agglomeration of obstacles
+    We suppose that we want one agglomeration of obstacles.
+    Factor of agglomeration = [0, 1] and moltiplied the number of obstacles in the agglomeration gives us 
+    the number of obstacles in the agglomeration.
+    The ramaining obstacles will be placed randomly in the grid (so it can happen
+    that they will be placed in the agglomeration as well, but it is not a problem)
+    """
+
+    directions = [(0,1), (0,-1), (1,0), (-1,0)]
+
     rows, cols = len(grid), len(grid[0])
-    obstacoles = set() 
+    agglomeration = set()
+
+    startR, startC = random.randint(0, rows-1), random.randint(0, cols-1)
+    agglomeration.add((startR, startC))
+    grid[startR][startC] = 1
+
+    while len(agglomeration) < nObstaclesInAgglomeration:
+        randomDirection = random.choice(directions)
+        randomCellInAgglomeration = random.choice(list(agglomeration))
+
+        nextR, nextC = randomCellInAgglomeration[0] + randomDirection[0], randomCellInAgglomeration[1] + randomDirection[1]
+        if 0 <= nextR < rows and 0 <= nextC < cols and (nextR, nextC) not in agglomeration:
+            agglomeration.add((nextR, nextC))
+            grid[nextR][nextC] = 1
+
+    return agglomeration
+    
+        
+def addObstacles(grid, nObstacle, agglomerationFactor):
+
+    rows, cols = len(grid), len(grid[0])
+
+    nObstaclesInAgglomeration = math.floor(agglomerationFactor * nObstacle)
+    obstacoles = createAgglomeration(grid, nObstaclesInAgglomeration) 
+
+    nObstacle = nObstacle - nObstaclesInAgglomeration
 
     for _ in range(nObstacle):
         r = random.randint(0, rows-1)
@@ -22,13 +58,13 @@ def addObstacles(grid, nObstacle):
 
     return grid
 
-def gridGenerator(rows, cols, freeCellRatio):
+def gridGenerator(rows, cols, freeCellRatio, agglomerationFactor):
     grid = [[0 for _ in range(rows)] for _ in range(cols)]
     
     #add obstacles to grid
     obstacleRatio = 1 - freeCellRatio
 
     nObstacle = math.floor(rows*cols*obstacleRatio)
-    grid = addObstacles(grid, nObstacle)
+    grid = addObstacles(grid, nObstacle, agglomerationFactor)
 
     return grid
--- pathfinding/main.py
+++ pathfinding/main.py
@@ -3,9 +3,10 @@ from generator.gridGenerator import gridGenerator
 from generator.graphGenerator import createGraphFromGrid
 from generator.pathsGenerator import createPaths
 
-nrows = 5
-ncols = 5
-freeCellRatio = 0.8
+nrows = 10
+ncols = 10
+freeCellRatio = 0.5
+agglomerationFactor = 0.5
 
 nAgents = 4
 max = freeCellRatio * nrows * ncols
@@ -15,12 +16,12 @@ maxIteration = 80
 maxRun = 4
 i = 0
 
-grid = gridGenerator(nrows,ncols, freeCellRatio)
+grid = gridGenerator(nrows,ncols, freeCellRatio, agglomerationFactor)
 graph = createGraphFromGrid(grid)
 paths = createPaths(nAgents, max, graph, maxIteration)
 
 while not paths and i < maxRun:
-    grid = gridGenerator(nrows,ncols, freeCellRatio)
+    grid = gridGenerator(nrows,ncols, freeCellRatio, agglomerationFactor)
     graph = createGraphFromGrid(grid)
     paths = createPaths(nAgents, max, graph, maxIteration)
     i += 1
