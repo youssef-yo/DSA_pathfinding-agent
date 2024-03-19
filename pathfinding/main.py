d2f0095c71c110a1d65efa07b28d93ffbc8d8210
Giorgio Bonardi <gio10black@gmail.com>
2024-03-19 10:41:42 +0100

    Refactoring of main

--- pathfinding/main.py
+++ pathfinding/main.py
@@ -1,7 +1,7 @@
 import argparse
 
-from UI.UI import run as runUI
-from UI.interactiveUI import run as runInteractiveUI
+from UI.UI import run as plot
+
 from UI.newUI import UI
 
 from generator.informationGenerator import Information
@@ -14,27 +14,34 @@ import random
 import numpy as np
 
 import configparser
-# seed = 10 to check waitGoalToBeFree (nAgent = 2)
+
+def getInputArgs():
+    parser = argparse.ArgumentParser(description="Pathfinding algorithm for multi-agent systems.")
+    parser.add_argument('--gui', action='store_true', help="Execute the program with the GUI interface. If not specified, the program will run in command line mode.")
+    parser.add_argument('--test', action='store_true', help="Evaluate the program using automated tests.")
+
+    return parser.parse_args()
+
 
 def setSeed(seed):
     random.seed(seed)
     np.random.seed(seed)
 
 
-def defaulParameterstValues():
-    global NROWS, NCOLS, FREE_CELL_RATIO, AGGLOMERATION_FACTOR, N_AGENTS, MAX, USE_REACH_GOAL_EXISTING_AGENTS, USE_RELAXED_PATH, SEED
-    NROWS = 10
-    NCOLS = 13
-    FREE_CELL_RATIO = 1
-    AGGLOMERATION_FACTOR = 0.3
-    MAX = 40
+# def defaulParameterstValues():
+#     global NROWS, NCOLS, FREE_CELL_RATIO, AGGLOMERATION_FACTOR, N_AGENTS, MAX, USE_REACH_GOAL_EXISTING_AGENTS, USE_RELAXED_PATH, SEED
+#     NROWS = 10
+#     NCOLS = 13
+#     FREE_CELL_RATIO = 1
+#     AGGLOMERATION_FACTOR = 0.3
+#     MAX = 40
 
-    N_AGENTS = 3
+#     N_AGENTS = 3
 
-    USE_RELAXED_PATH = True
-    USE_REACH_GOAL_EXISTING_AGENTS = False
+#     USE_RELAXED_PATH = True
+#     USE_REACH_GOAL_EXISTING_AGENTS = False
 
-    SEED = 22
+#     SEED = 22
 
 def readParametersFromFile():
     global NROWS, NCOLS, FREE_CELL_RATIO, AGGLOMERATION_FACTOR, N_AGENTS, MAX, USE_REACH_GOAL_EXISTING_AGENTS, USE_RELAXED_PATH, SEED
@@ -55,51 +62,57 @@ def readParametersFromFile():
     SEED = int(config['SEED']['seed'])
     
 
-def getParameters():
-    defaulParameterstValues()
-    #readParametersFromFile()
+def executeCLI():
+    # Start time and memory monitoring
+    information = Information(SEED)
+    information.startMonitoring()
+
+    instance = generateInstance(NROWS, NCOLS, FREE_CELL_RATIO, AGGLOMERATION_FACTOR, N_AGENTS, MAX, USE_REACH_GOAL_EXISTING_AGENTS, USE_RELAXED_PATH)
+
+    if not instance:
+        print("Parameter max was not valid for the current configuration.\n Parameters too restrictive, try again with different ones.")
+    else:
+        print(" ------------- ")
+        print("NEW AGENT (init, goal): (", instance.getInit(), ", ", instance.getGoal(), ")")
+        
+        path, minimumSpanningTree, closedSet = reachGoal(instance, USE_RELAXED_PATH)
+        
+        if not path:
+            print("No path found for new agent")
+        else:
+            print("!!!! Path found for new agent")
+            path.printPath()
+
+        if path:
+            instance.addPath(path)
+
+            information.stopMonitoring()
+            information.setValues(instance, FREE_CELL_RATIO, AGGLOMERATION_FACTOR, path, minimumSpanningTree, closedSet, USE_RELAXED_PATH, USE_REACH_GOAL_EXISTING_AGENTS)
+            
+            information.printInformation()
+            information.saveInformationToFile()
+
+            # plot(instance.getGrid(), instance.getPaths(), minimumSpanningTree)
+
+def executeUI():
+    ui = UI(generateInstance, reachGoal, informationGenerator, SEED) #TODO: create class for the two controllers
+    ui.run()
+
+def executeTest():
+    pass
 
 def main():
-    getParameters()
+    readParametersFromFile()
     setSeed(SEED)
-    parser = argparse.ArgumentParser(description="Pathfinding algorithm for multi-agent systems.")
-    parser.add_argument('--gui', action='store_true', help="Execute the program with the GUI interface. If not specified, the program will run in command line mode.")
-    args = parser.parse_args()
+
+    args = getInputArgs()
 
     if args.gui:
-        ui = UI(generateInstance, reachGoal, informationGenerator, SEED) #TODO: create class for the two controllers
-        ui.run()
+        executeUI()
+    elif args.test:
+        executeTest()
     else:
-        # Start time and memory monitoring
-        information = Information(SEED)
-        information.startMonitoring()
-
-        instance = generateInstance(NROWS, NCOLS, FREE_CELL_RATIO, AGGLOMERATION_FACTOR, N_AGENTS, MAX, USE_REACH_GOAL_EXISTING_AGENTS, USE_RELAXED_PATH)
-
-        if not instance:
-            print("Parameter max was not valid for the current configuration.\n Parameters too restrictive, try again with different ones.")
-        else:
-            print(" ------------- ")
-            print("NEW AGENT (init, goal): (", instance.getInit(), ", ", instance.getGoal(), ")")
-            
-            path, minimumSpanningTree, closedSet = reachGoal(instance, USE_RELAXED_PATH)
-            
-            if not path:
-                print("No path found for new agent")
-            else:
-                print("!!!! Path found for new agent")
-                path.printPath()
-
-            if path:
-                instance.addPath(path)
-
-                information.stopMonitoring()
-                information.setValues(instance, FREE_CELL_RATIO, AGGLOMERATION_FACTOR, path, minimumSpanningTree, closedSet, USE_RELAXED_PATH, USE_REACH_GOAL_EXISTING_AGENTS)
-                
-                information.printInformation()
-                information.saveInformationToFile()
-
-                # runUI(instance.getGrid(), instance.getPaths(), minimumSpanningTree)
+        executeCLI()
            
 
 if __name__ == "__main__":
